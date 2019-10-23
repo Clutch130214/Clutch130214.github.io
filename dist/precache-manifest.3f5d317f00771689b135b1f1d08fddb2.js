943ee8247c3699946b70e8c97c5c14628f497344
Bastien@MSI <chauveaubastien@gmail.com>
2019-10-23 09:18:33 +0200

    first dit

---
+++ dist/precache-manifest.3f5d317f00771689b135b1f1d08fddb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a0f095384a4ead12a9",
+    "url": "/css/app.70fa1f38.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c72b1c556e8acdd4f234854aaf39cda0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8a5831f4e54ff48bc0d",
+    "url": "/js/about.e97b8d0b.js"
+  },
+  {
+    "revision": "66a0f095384a4ead12a9",
+    "url": "/js/app.9382e003.js"
+  },
+  {
+    "revision": "18a8dff35c2ee226a844",
+    "url": "/js/chunk-vendors.d47cf30f.js"
+  },
+  {
+    "revision": "a8c976ce1df196bdf9bbe5f3cfb86bee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
