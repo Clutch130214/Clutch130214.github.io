d02d7254b9873a29fc55d049ee27e99358291da9
Bastien@MSI <chauveaubastien@gmail.com>
2019-10-28 20:35:13 +0100

    update dist

---
+++ precache-manifest.88e511a267c4d300e7bc728992ca6e4d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2938dcd132b36aa54d1",
+    "url": "/css/about.de235827.css"
+  },
+  {
+    "revision": "e72fa00bf7dd6117dbba",
+    "url": "/css/app.02923c83.css"
+  },
+  {
+    "revision": "0c63c6d818710ce23f46",
+    "url": "/css/chunk-vendors.528f884b.css"
+  },
+  {
+    "revision": "a01a4a2b9f173165ad8d3c60f8a2a47d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2938dcd132b36aa54d1",
+    "url": "/js/about.0a6dbfea.js"
+  },
+  {
+    "revision": "e72fa00bf7dd6117dbba",
+    "url": "/js/app.b91ca997.js"
+  },
+  {
+    "revision": "0c63c6d818710ce23f46",
+    "url": "/js/chunk-vendors.3490fab4.js"
+  },
+  {
+    "revision": "a8c976ce1df196bdf9bbe5f3cfb86bee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c3bca96647a95a964423b43056669a9b",
+    "url": "/survey.ico"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee361aef0e3b7924e6e14e222e196974.js"
+  "/precache-manifest.88e511a267c4d300e7bc728992ca6e4d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "app-questionnaire-vue"});
